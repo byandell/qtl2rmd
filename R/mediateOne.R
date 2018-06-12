7b903425a5b7f9f1cf018240f3571285819bf52a
Brian Yandell <byandell@wisc.edu>
2018-06-12 12:26:26 -0500

    add mediateOne.R and mediateOne.sh

--- R/mediateOne.R
+++ R/mediateOne.R
@@ -1,9 +1,7 @@
 params <- list(
   target_name = "GLP1_G83_ins_secrete_gm",
-  chr_id = "16",
-  mediator_name = "",
-  mediator_id = "",
-  pos_Mbp = 90,
+  chr_id = "1",
+  pos_Mbp = NULL,
   dataSetup = "R/dataJaxMadison.R",
   snpScan = 10,
   offset = 2,
@@ -21,8 +19,6 @@ for(i in names(params)) {
 
 target_name <- params$target_name
 chr_id <- as.character(params$chr_id)
-mediator_id <- params$mediator_id
-mediator_name <- params$mediator_name
 
 datapath <- params$datapath
 
@@ -40,10 +36,6 @@ source(dataSetup)
 target <- as.matrix(pheno_data[, target_name, drop = FALSE])
 rm(pheno_data)
 addcovar <- qtl2pattern::covar_df_mx(covar)
-
-map <- query_probs(chr_id)
-genoprobs <- map$probs
-map <- map$map
 kinship <- kinship[[chr_id]]
 
 ### Peak position for target
@@ -55,6 +47,7 @@ peaks <- readxl::read_excel(file.path(datapath,"Ex vivo peak summary with QTL co
 pos_Mbp <- peaks$pos[1]
 if(is.na(pos_Mbp)) {
   pos_Mbp <- params$pos_Mbp
+  stopifnot(!is.null(pos_Mbp))
 }
 cat("using peak position", pos_Mbp, "\n", file = stderr())  
 
@@ -66,197 +59,81 @@ if(is.na(snpScan) || snpScan <= 0) {
   end <- pos_Mbp + snpScan
 }
 
+map <- query_probs(chr_id, start, end)
+genoprobs <- map$probs
+map <- map$map
+
 ### Mediator information
 
 mrna <- query_mrna(chr_id)
 
-# Must supply one of mediator id or name.
-if(mediator_id != "") {
-  mrna_id <- mediator_id
-  if(mediator_name != "") {
-    mrna_name <- 
-      (mrna$annot %>%
-         filter(id == mediator_id))$symbol
-    stopifnot(mediator_name == mrna_name)
-  }
-} else {
-  stopifnot(mediator_name != "")
-  mrna_id <- 
-    (mrna$annot %>%
-       filter(symbol == mediator_name))$id
-  stopifnot(length(mrna_id) == 1)
-}
+# Find peaks for local mRNA that are close to SNP peak.
 
-mrna_pos <- 
-  (mrna$peaks %>%
-     filter(gene_id == mrna_id, 
-            qtl_chr == chr_id))$qtl_pos
-mrna.expr <- as.matrix(mrna$expr[, mrna_id, drop = FALSE])
-mrna.annot <- 
-  dplyr::filter(
-    mrna$annot,
-    id == mrna_id)
+med_signif <- 
+  (dplyr::filter(
+    mrna$peaks,
+    qtl_chr == chr_id,
+    abs(qtl_pos - pos_Mbp) <= params$snpScan,
+    gene_start >= pos_Mbp - params$snpScan,
+    gene_end <= pos_Mbp + params$snpScan))$gene_id
+
+mrna.expr <- mrna$expr[, med_signif, drop = FALSE]
+m <- match(med_signif, mrna$annot$id)
+mrna.annot <- mrna$annot[m,, drop = FALSE]
 rm(mrna)
 
-# Association mapping for causal models
+#################################################
 
-# Reduce to common IDs
-m <- qtl2::get_common_ids(genoprobs,
-                          target,
-                          addcovar,
-                          mrna.expr,
-                          complete.cases = TRUE)
-genoprobs <- subset(genoprobs, ind = m)
-target <- target[m,, drop = FALSE]
-mediator <- mrna.expr[m,, drop = FALSE]
-kinship <- qtl2::decomp_kinship(kinship[m,m])
-addcovar <- addcovar[m,, drop=FALSE]
+out <- list()
 
-t.m <- qtl2::fit1(cbind(1, as.matrix(mediator)),
-                  target,
-                  kinship,
-                  addcovar)
-nind <- length(t.m$ind_lod)
-t.m <- t.m$lod
-m.t <- qtl2::fit1(cbind(1, as.matrix(target)),
-                  mediator,
-                  kinship,
-                  addcovar)$lod
-
-assoc_tar = qtl2::scan1snps(genoprobs = genoprobs[,chr_id], map = map, 
-                      pheno = target,
-                      kinship = kinship,
-                      addcovar = addcovar, chr = chr_id, start = start,
-                      end = end, query_func = query_variant, cores = 4,
-                      keep_all_snps = FALSE)
-assoc_med = qtl2::scan1snps(genoprobs = genoprobs[,chr_id], map = map, 
-                      pheno = mediator,
-                      kinship = kinship,
-                      addcovar = addcovar, chr = chr_id, start = start,
-                      end = end, query_func = query_variant, cores = 4,
-                      keep_all_snps = FALSE)
-assoc_tar_med = qtl2::scan1snps(genoprobs = genoprobs[,chr_id], map = map, 
-                      pheno = target,
-                      kinship = kinship,
-                      addcovar = cbind(addcovar, mediator), chr = chr_id, start = start,
-                      end = end, query_func = query_variant, cores = 4,
-                      keep_all_snps = FALSE)
-assoc_cmst <- assoc_tar
-assoc_cmst$lod <- cbind(assoc_med$lod, assoc_tar$lod, assoc_tar$lod,
-                        assoc_tar_med$lod, assoc_tar$lod)
-colnames(assoc_cmst$lod) <- c("causal","reactive","independent","undecided","mediation")
-assoc_cmst$lod[,1] <- t.m + assoc_med$lod[,1]
-assoc_cmst$lod[,2] <- m.t + assoc_tar$lod[,1]
-assoc_cmst$lod[,3] <- assoc_tar$lod[,1] + assoc_med$lod[,1] 
-assoc_cmst$lod[,4] <- assoc_tar_med$lod[,1] + t.m + assoc_med$lod[,1] 
-assoc_cmst$lod[,5] <- assoc_tar$lod[,1] - assoc_tar_med$lod[,1]
-
-rm(assoc_tar, assoc_med, assoc_tar_med)
-invisible(gc(verbose = FALSE))
-
-nmar_snp <- 3
-penalty_snp <- c(nmar_snp, nmar_snp, 2*(nmar_snp-1), 2*nmar_snp-1,0) * log10(nind) / 2
-
-tmp <- assoc_cmst$lod -
-  matrix(penalty_snp, nrow(assoc_cmst$lod), ncol(assoc_cmst$lod), byrow = TRUE) +
-  penalty_snp[1]
-
-ts <- qtl2pattern::top_snps_all(tmp, assoc_cmst$snpinfo) %>%
-   group_by(pheno) %>%
-   summarize(minpos = min(pos[lod == max(lod)]),
-             maxpos = max(pos[lod == max(lod)]),
-             snp_id = snp_id[which.max(lod)][1],
-             sdp = paste(sort(unique(sdp[lod == max(lod)], collapse = ","))),
-             lod = max(lod)) %>%
-  arrange(desc(lod))
-
-## Mediation test with best SNPs
-
-sp <- qtl2::genoprob_to_snpprob(genoprobs, assoc_cmst$snpinfo)[[chr_id]][,, ts$snp_id[1]]
-
-tar_test <- intermediate::mediation_test(
-  target = target,
-  mediator = mediator,
-  annotation = mrna.annot,
-  covar_tar = covar,
-  covar_med = covar,
-  kinship = kinship,
-  driver = sp)
-
-tar_best <- summary(tar_test) %>%
-  select(symbol,chr,pvalue,triad,mediation,LR) %>%
-  rename(lod = "LR") %>%
-  mutate(lod = lod / log(10),
-         mediation = mediation / log(10))
-
-# Mediation test over interval
-
-sp <- qtl2::genoprob_to_snpprob(genoprobs, assoc_cmst$snpinfo)[[chr_id]]
-
-mediators <- mediator[, rep(mrna_id, dim(sp)[3]), drop = FALSE]
-colnames(mediators) <- dimnames(sp)[[3]]
-
-m <- match(mrna_id, mrna.annot$id)
-annotation <- mrna.annot[rep(m, ncol(mediators)),]
-annotation$id <- colnames(mediators)
-annotation$driver <- colnames(mediators)
-annotation$pos <- assoc_cmst$snpinfo$pos
-annotation$qtl_pos <- NULL
-annotation$qtl_lod <- NULL
-
-# Within interval already selected, 
-#   pick the region of maximum joint LOD (within 1.5 of peak).
-#   run mediation test and find the best models (using BIC among the four models)
-#   report out pvalues.
-
-m <- which(assoc_cmst$lod[,"undecided"] >= max(assoc_cmst$lod[,"undecided"]) - 1.5)
-rng <- range(assoc_cmst$snpinfo$pos[m])
-m <- which(annotation$pos >= rng[1] & annotation$pos <= rng[2])
-
-med_test_scan <- intermediate::mediation_test(
-    target = target,
-    mediator = mediators[,m],
-    annotation = annotation[m,],
-    covar_tar = covar,
-    covar_med = covar,
-    kinship = kinship,
-    driver = NULL,
-    driver_med = sp[,,m])
-
-best_snp <-
-  (med_test_scan$test %>%
-     filter(model == "undecided",
-            LR >= max(LR) - 1.5 * log(10)))$id
-tmp <- med_test_scan$best
-m <- match(best_snp, tmp$id)
-best_snp <- tmp[m,]
-m <- match(best_snp$id, assoc_cmst$snpinfo$snp_id)
-best_snp$sdp <- assoc_cmst$snpinfo$sdp[m]
-best_snp <- 
-  best_snp %>%
-  arrange(pvalue) %>%
-  mutate(pattern = qtl2pattern::sdp_to_pattern(sdp, LETTERS[1:8]))
-
-topsnps <- 
-  qtl2::index_snps(map,
-    query_variant(chr_id, start, end)) %>%
-  filter(pos >= min(best_snp$pos),
-         pos <= max(best_snp$pos),
-         sdp %in% unique(best_snp$sdp))
-m <- match(best_snp$id, topsnps$snp_id)
-best_snp$index <- topsnps$index[m]
-topsnps <- left_join(topsnps,
-                     best_snp %>%
-                       select(-lod) %>%
-                       rename(lod = "LR") %>%
-                       mutate(lod = lod / log(10)) %>%
-                       select(index, triad, lod, pvalue, symbol),
-                     by = "index")
-
-if(resultpath != "") {
-  write.csv(
-    topsnps, 
-    file = file.path(
-      resultpath, 
-      paste0(target_name, "_", chr_id, "_", mediator_name, "_topsnps.csv")))
+for(mediator_id in med_signif) {
+  mediator_name <- (mrna.annot %>% filter(id == mediator_id))$symbol
+  mediator <- as.matrix(mrna$expr[, mediator_id, drop = FALSE])
+  annotation <- 
+    dplyr::filter(
+      mrna.annot,
+      id == mediator_id)
+  
+  # Association mapping for causal models
+  
+  # Reduce to common IDs
+  m <- qtl2::get_common_ids(genoprobs,
+                            target,
+                            addcovar,
+                            complete.cases = TRUE)
+  genoprobs <- subset(genoprobs, ind = m)
+  target <- target[m,, drop = FALSE]
+  kinship <- kinship[m,m]
+  addcovar <- addcovar[m,, drop=FALSE]
+  
+  # Mediation test on SNPs in peak region.
+  if(params$SNPlevels == 3) {
+    genoprobs <- query_probs(chr = chr_id, start = start, stop = end, allele = FALSE)
+    map <- genoprobs$map
+    genoprobs <- genoprobs$probs
+  } else {
+    genoprobs <- query_probs(chr = chr_id, start = start, stop = end)
+    map <- genoprobs$map
+    genoprobs <- genoprobs$probs
+  }
+  
+  out[[mediator_id]] <- 
+    intermediate::mediation_qtl2(target,
+                                 mediator,
+                                 annotation, covar, covar, kinship,
+                                 genoprobs, map,
+                                 drop_lod = 1.5, query_variant,
+                                 cores = 4)
 }
+
+saveRDS(out, 
+        file = file.path(resultpath,
+                         paste0(target_name, "_", chr_id, "_med_qtl2.rds")))
+write.csv(
+  med_index$best %>%
+    select(-lod) %>%
+    rename(lod = "LR") %>%
+    mutate(lod = lod / log(10)),
+  file = file.path(
+    resultpath, 
+    paste0(target_name, "_", chr_id, "_", "_topsnps.csv")))
---
+++ slurm/mediateOne.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL # send us an email about all events
+#SBATCH --mail-user=brian.yandell@wisc.edu
+# SBATCH this is a comment
+#SBATCH -J test_mediateQtl2 # give the job a name
+#SBATCH -t 1:00:00 # run this simulation for 5 days (HOURS:MINUTES:SECONDS)
+#SBATCH -p short # send this job to the long partition since we want it to run more than 4 days
+#SBATCH --mem-per-cpu=500M
+#SBATCH --cpus-per-task=4
+#SBATCH -n 1
+# Load R version 3.4.4
+module load R/R-3.4.4
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export R_LIBS_USER=/workspace2/yandell/Rlib
+# Run our R program myjob.R which resides in /workspace/[username]/
+cd /workspace2/yandell
+R CMD BATCH --no-save R/mediateOne.R test_mediateOne.out
